9c72348055f79869b4994417cae1d9e7f3fafc86
Young <liyang_bsy@hotmail.com>
2015-08-14 10:09:49 +0800

    update

--- src/net/vicp/lylab/lyserver/ServerRuntime.java
+++ src/net/vicp/lylab/lyserver/ServerRuntime.java
@@ -2,15 +2,13 @@ package net.vicp.lylab.lyserver;
 
 import java.io.File;
 
-import net.vicp.lylab.core.BaseObject;
 import net.vicp.lylab.core.CoreDef;
 import net.vicp.lylab.core.GlobalInitializer;
 import net.vicp.lylab.utils.Config;
 
-public class ServerRuntime extends BaseObject {
+public class ServerRuntime {
 	public static void main(String[] arg) {
 		CoreDef.config = new Config(CoreDef.rootPath + File.separator + "config" + File.separator + "config.txt");
-		CoreDef.config.getString("fdsa");
 		GlobalInitializer.createInstance();
 	}
 	
