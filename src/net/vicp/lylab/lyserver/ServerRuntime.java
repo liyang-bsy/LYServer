8ac9d3639517a88408cd1c1e4e5213c7454f9289
Young <liyang_bsy@hotmail.com>
2015-09-08 15:42:48 +0800

    give_up

--- src/net/vicp/lylab/lyserver/ServerRuntime.java
+++ src/net/vicp/lylab/lyserver/ServerRuntime.java
@@ -3,17 +3,15 @@ package net.vicp.lylab.lyserver;
 import java.io.File;
 
 import net.vicp.lylab.core.CoreDef;
-import net.vicp.lylab.core.GlobalInitializer;
 import net.vicp.lylab.utils.Config;
 
 public class ServerRuntime {
 	public static void main(String[] arg) {
 		CoreDef.config = new Config(CoreDef.rootPath + File.separator + "config" + File.separator + "config.txt");
-		GlobalInitializer.createInstance();
 	}
 	
 	public static void close() {
-		GlobalInitializer.destroyInstance();
+		CoreDef.config.deepClose();
 	}
 	
 }
