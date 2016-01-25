07c48d236f50a637aa9056009e04e5ff9e4124ab
Young <liyang_bsy@hotmail.com>
2016-01-25 15:12:55 +0800

    with session

--- src/net/vicp/lylab/server/ServerRuntime.java
+++ src/net/vicp/lylab/server/ServerRuntime.java
@@ -9,6 +9,7 @@ public class ServerRuntime {
 		try {
 			CoreDef.config.reload(CoreDef.rootPath + File.separator + "config" + File.separator + "config.txt");
 		} catch (Exception e) {
+			e.printStackTrace();
 			System.exit(-1);
 		}
 	}
--- src/net/vicp/lylab/server/dispatcher/LoggedKeyDispatcher.java
+++ src/net/vicp/lylab/server/dispatcher/LoggedKeyDispatcher.java
@@ -1,11 +1,12 @@
-package net.vicp.lylab.server.aop;
+package net.vicp.lylab.server.dispatcher;
 
 import net.vicp.lylab.core.CoreDef;
 import net.vicp.lylab.core.model.Message;
 import net.vicp.lylab.core.model.RPCMessage;
+import net.vicp.lylab.server.dispatcher.SimpleKeyDispatcher;
 import net.vicp.lylab.server.utils.Logger;
 
-public class LoggedKeyDispatcherAop extends SimpleKeyDispatcherAop<Message> {
+public class LoggedKeyDispatcher extends SimpleKeyDispatcher<Message> {
 	@Override
 	protected void logger(Message request, Message response) {
 		System.out.println("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
