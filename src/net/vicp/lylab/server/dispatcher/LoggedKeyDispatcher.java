729a4279963e1f3e1b628fb09c47c30e4cddf71c
Young <liyang_bsy@hotmail.com>
2016-01-27 17:13:43 +0800

    bug fixed

--- src/net/vicp/lylab/server/dispatcher/LoggedKeyDispatcher.java
+++ src/net/vicp/lylab/server/dispatcher/LoggedKeyDispatcher.java
@@ -2,18 +2,14 @@ package net.vicp.lylab.server.dispatcher;
 
 import net.vicp.lylab.core.CoreDef;
 import net.vicp.lylab.core.model.Message;
-import net.vicp.lylab.core.model.RPCMessage;
-import net.vicp.lylab.server.dispatcher.SimpleKeyDispatcher;
 import net.vicp.lylab.server.utils.Logger;
 
 public class LoggedKeyDispatcher extends SimpleKeyDispatcher<Message> {
 	@Override
 	protected void logger(Message request, Message response) {
-//		System.out.println("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
-//				+ "\nBefore:" + request + "\nAfter:" + response);
 		((Logger) CoreDef.config.getConfig("Singleton").getObject("Logger"))
-				.appendLine("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
-						+ "\nBefore:" + request + "\nAfter:" + response);
+				.appendLine("Access key:" + request.getKey() + 
+						"\nBefore:" + request + "\nAfter:" + response);
 	}
 
 }
