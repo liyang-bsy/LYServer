aafe1f356f6e17cf18a5a7757a0ddf656ee9ac3a
Young <liyang_bsy@hotmail.com>
2016-02-22 11:20:31 +0800

    up

--- src/net/vicp/lylab/server/ServerRuntime.java
+++ src/net/vicp/lylab/server/ServerRuntime.java
@@ -8,14 +8,17 @@ public class ServerRuntime {
 	public static void main(String[] arg) {
 		try {
 			CoreDef.config.reload(CoreDef.rootPath + File.separator + "config" + File.separator + "config.txt");
+			System.out.println("Server started");
 		} catch (Exception e) {
 			e.printStackTrace();
 			close();
+			System.out.println("Server failed");
 		}
 	}
 	
 	public static void close() {
 		CoreDef.config.deepClose();
+		System.out.println("Server stopped");
 	}
 	
 }
